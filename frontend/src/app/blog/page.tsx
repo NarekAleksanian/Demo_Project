082b3b8339dba4e463d431625ffba9fa1d8b2a90
Narek Aleksanyan <narek.aleksanian01@gmail.com>
2024-10-23 11:54:19 +0400

    blog

---
+++ frontend/src/app/blog/page.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+import React from "react";
+
+const BlogPage: React.FC = () => {
+    return (
+        <div>
+            <h1>Welcome to the Blog</h1>
+            {/* Blog content can be added here */}
+        </div>
+    );
+};
+
+export default BlogPage;
