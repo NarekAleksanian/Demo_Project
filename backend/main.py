3ce3b8fb9a2658ab80aa054c8dc4e532a9db0296
Narek Aleksanyan <narek.aleksanian01@gmail.com>
2024-11-09 06:30:03 +0400

    use settings for run

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,12 @@
 import uvicorn
 from fastapi import FastAPI
+from backend.core.config import settings
 
 app = FastAPI()
 if __name__ == "__main__":
-    uvicorn.run("main:app", reload=True)
+    uvicorn.run(
+        "main:app",
+        host=settings.run.host,
+        port=settings.run.port,
+        reload=True,
+    )
