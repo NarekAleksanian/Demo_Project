7c54f9823feff5b36da3c2ada23c618bed04ff94
Narek Aleksanyan <narek.aleksanian01@gmail.com>
2024-11-10 21:05:55 +0400

    camel-case + db-helper-dispose + declarative-base

---
+++ backend/core/__init__.py
@@ -0,0 +1,7 @@
+__all__ = (
+    "db_helper",
+    "Base",
+)
+
+from backend.core.models.db_helper import db_helper
+from backend.core.models.base import Base
---
+++ backend/core/models/__init__.py
@@ -0,0 +1,5 @@
+__all__ = (
+    "db_helper",
+)
+
+from backend.core.models.db_helper import db_helper
