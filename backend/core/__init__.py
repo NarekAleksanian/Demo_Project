f1e1fb0bc6cc5f8de2cb4786dc9d2604c496043e
Narek Aleksanyan <narek.aleksanian01@gmail.com>
2024-11-10 21:44:19 +0400

    alembic installation + black

--- backend/core/__init__.py
+++
@@ -1,7 +0,0 @@
-__all__ = (
-    "db_helper",
-    "Base",
-)
-
-from backend.core.models.db_helper import db_helper
-from backend.core.models.base import Base
--- backend/core/models/__init__.py
+++ backend/core/models/__init__.py
@@ -1,5 +1,9 @@
 __all__ = (
     "db_helper",
+    "Base",
+    # "User"
 )
 
 from backend.core.models.db_helper import db_helper
+from backend.core.models.base import Base
+# from backend.core.models.user import User
